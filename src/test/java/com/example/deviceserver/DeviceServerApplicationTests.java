8a8a3bfaf93bf0e9b39c87e06766eeaf4d3728ee
PUO <86181292+kl204@users.noreply.github.com>
2024-05-20 19:39:07 +0900

    first commit

---
+++ src/test/java/com/example/deviceserver/DeviceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deviceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
