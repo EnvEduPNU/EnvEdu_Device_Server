ac699a2f1808f3415e63a5eac48f4ca824dd1ab9
PUO <86181292+kl204@users.noreply.github.com>
2024-05-30 17:11:41 +0900

    [update] 컨트롤러 테스트3 #1

--- src/main/java/com/example/deviceserver/controller/TestController.java
+++ src/main/java/com/example/deviceserver/controller/TestController.java
@@ -1,13 +1,14 @@
 package com.example.deviceserver.controller;
 
-import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/test")
     public ResponseEntity<?> testMethod() {
         return ResponseEntity.ok().body("yes");  // HTTP 200 OK 상태 코드 반환
