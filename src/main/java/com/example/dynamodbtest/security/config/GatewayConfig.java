d6a19db67eee456aed2ca68c92a2741f1064eed6
PUO <86181292+kl204@users.noreply.github.com>
2024-06-20 15:44:08 +0900

    [update] Session 라우팅 수정 #1

--- src/main/java/com/example/dynamodbtest/security/config/GatewayConfig.java
+++ src/main/java/com/example/dynamodbtest/security/config/GatewayConfig.java
@@ -35,7 +35,7 @@ public class GatewayConfig {
         log.info("Gateway 설정 완료!");
 
         return builder.routes()
-                .route("example_route", r -> r.path("/login/**","/seed/**","/mydata/**","/datafolder/**")
+                .route("example_route", r -> r.path("/login/**","/seed/**","/mydata/**","/datafolder/**","/register-session/**","/get-session-ids/**")
                         .filters(f -> f.modifyRequestBody(String.class, String.class, (exchange, s) -> {
                                             ServerHttpRequest request = exchange.getRequest();
                                             ServerHttpRequest.Builder requestBuilder = request.mutate();
