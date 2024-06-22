a39b315b36bac0fb70398fd0e9f640a72f747782
PUO <86181292+kl204@users.noreply.github.com>
2024-06-22 16:31:52 +0900

    [update] 화면 공유 cors 및 routes #1

--- src/main/java/com/example/dynamodbtest/security/config/SecurityConfig.java
+++ src/main/java/com/example/dynamodbtest/security/config/SecurityConfig.java
@@ -67,6 +67,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("https://new.greenseed.or.kr"));  // 허용할 출처
+//        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));  // 허용할 출처
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));  // 허용할 HTTP 메소드
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept", "X-Requested-With"));  // 허용할 헤더
         configuration.setExposedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));  // 브라우저에서 접근 가능한 헤더
