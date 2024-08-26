6d1414dca9c9c498aabd7c88071b9cc41cec9082
PUO <86181292+kl204@users.noreply.github.com>
2024-08-26 22:59:16 +0900

    [update] 보고서 메서드 개발

---
+++ src/main/java/com/example/dynamodbtest/dynamodb/createlecture/service/ReportStepContentServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.dynamodbtest.dynamodb.createlecture.service;
+
+import com.example.dynamodbtest.dynamodb.createlecture.entity.ReportStepContent;
+import com.example.dynamodbtest.dynamodb.createlecture.repository.ReportStepContentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+@Service
+public class ReportStepContentServiceImpl implements ReportStepContentService {
+
+    private final ReportStepContentRepository reportStepContentRepository;
+
+    @Override
+    public Mono<Void> saveStepContents(ReportStepContent reportStepContent) {
+        return reportStepContentRepository.save(reportStepContent).then();
+    }
+
+    @Override
+    public Flux<ReportStepContent> getAllStepContents() {
+        return reportStepContentRepository.findAll();
+    }
+
+    @Override
+    public Mono<Void> deleteContent(String uuid, String timestamp) {
+        return reportStepContentRepository.deleteByStepName(uuid,timestamp);
+
+    }
+
+    @Override
+    public Mono<Void> updateStepContents(String uuid, String timestamp, ReportStepContent reportStepContent) {
+        return reportStepContentRepository.updateStepContent(uuid,timestamp,reportStepContent);
+    }
+
+    @Override
+    public Flux<ReportStepContent> getStepContentByUuid(String uuid) {
+        return  reportStepContentRepository.getStepContentByUuid(uuid);
+    }
+}
