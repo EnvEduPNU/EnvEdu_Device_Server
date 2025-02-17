5763033a6d42a3f68d48bec0c4d823cca5ad39db
PUO <86181292+kl204@users.noreply.github.com>
2025-02-17 16:43:31 +0900

    [update] Researcher Role 개발

---
+++ src/main/java/com/example/dynamodbtest/user/dto/Researcher.java
@@ -0,0 +1,35 @@
+package com.example.dynamodbtest.user.dto;
+
+
+import com.example.dynamodbtest.user.enumerate.Gender;
+import com.example.dynamodbtest.user.enumerate.IsAuthorized;
+import com.example.dynamodbtest.user.enumerate.Role;
+import com.example.dynamodbtest.user.enumerate.State;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDate;
+
+
+@Table("researcher")
+@Getter
+@Setter
+@ToString
+public class Researcher extends User {
+
+    @Builder(builderMethodName = "researcherBuilder")
+    public Researcher(String username, String password, String email, LocalDate birthday, Role role, Gender gender, State state, String nickname, IsAuthorized isAuthorized)
+    {
+        super(username, password, email, birthday, role, gender, state, nickname);
+        this.isAuthorized = isAuthorized;
+    }
+
+    @Column("is_authorized")
+    private IsAuthorized isAuthorized;
+
+}
+
--- src/main/java/com/example/dynamodbtest/user/enumerate/Role.java
+++ src/main/java/com/example/dynamodbtest/user/enumerate/Role.java
@@ -2,7 +2,8 @@ package com.example.dynamodbtest.user.enumerate;
 
 import com.example.dynamodbtest.user.dto.Educator;
 import com.example.dynamodbtest.user.dto.Student;
-import com.example.dynamodbtest.user.dto.Admin; // Admin 클래스 임포트
+import com.example.dynamodbtest.user.dto.Admin;
+import com.example.dynamodbtest.user.dto.Researcher;
 import com.example.dynamodbtest.user.dto.User;
 import com.example.dynamodbtest.user.dto.request.RegisterDTO;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -43,6 +44,21 @@ public enum Role {
         @Override
         public User generateUserByRole(RegisterDTO registerDTO, PasswordEncoder passwordEncoder) {
             return Admin.adminBuilder() // Admin 객체 생성
+                    .username(registerDTO.getUsername())
+                    .password(passwordEncoder.encode(registerDTO.getPassword()))
+                    .email(registerDTO.getEmail())
+                    .gender(registerDTO.getGender())
+                    .role(registerDTO.getRole())
+                    .state(State.ACTIVE)
+                    .birthday(registerDTO.getBirthday().toLocalDate())
+                    .nickname(registerDTO.getNickname())
+                    .build();
+        }
+    },
+    ROLE_RESEARCHER {
+        @Override
+        public User generateUserByRole(RegisterDTO registerDTO, PasswordEncoder passwordEncoder) {
+            return Researcher.researcherBuilder()
                     .username(registerDTO.getUsername())
                     .password(passwordEncoder.encode(registerDTO.getPassword()))
                     .email(registerDTO.getEmail())
